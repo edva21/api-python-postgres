5615f94755af2910eb7e93434c0fd78f33b9cae2
Eddy V <edva59012@outlook.com>
2019-03-26 13:57:28 -0600

    added vendor type with read working

---
+++ vendor.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import psycopg2
+import json
+from config import config
+
+app = Flask(__name__)
+api = Api(app)
+
+users = [
+    {
+        "name": "Nicholas",
+        "age": 42,
+        "occupation": "Network Engineer"
+    },
+    {
+        "name": "Elvin",
+        "age": 32,
+        "occupation": "Doctor"
+    },
+    {
+        "name": "Jass",
+        "age": 22,
+        "occupation": "Web Developer"
+    }
+]
+
+class Vendor(Resource):
+    def get(self, vendor_id):
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a cursor object for execution
+        cur = conn.cursor()
+        # another way to call a stored procedure
+        # cur.execute("SELECT * FROM get_parts_by_vendor( %s); ",(vendor_id,))
+        cur.callproc('get_vendor', vendor_id)
+        # process the result set
+        row = cur.fetchone()
+        results = []
+        columns = ('vendor_id', 'vendor_name');
+        while row is not None:
+            results.append(dict(zip(columns,row)))
+            row = cur.fetchone()
+        # close the communication with the PostgreSQL database server
+        cur.close()
+        return results,200
+    def post(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                return "User with name {} already exists".format(name), 400
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                user["age"] = args["age"]
+                user["occupation"] = args["occupation"]
+                return user, 200
+        
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
+
+    def delete(self, name):
+        global users
+        users = [user for user in users if user["name"] != name]
+        return "{} is deleted.".format(name), 200
+      
+api.add_resource(Vendor, "/vendor/<string:vendor_id>")
+
+app.run(debug=True)
